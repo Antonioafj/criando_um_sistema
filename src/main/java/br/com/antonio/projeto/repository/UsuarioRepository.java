888e7385d59e6e8b17da6d5b9d2238337b826f05
Antonioafj <antonioafj.edu@gmail.com>
2024-07-28 17:15:45 -0400

    criando classes repository dto criando CRUD em services

---
+++ src/main/java/br/com/antonio/projeto/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package br.com.antonio.projeto.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.antonio.projeto.entity.UsuarioEntity;
+
+public interface UsuarioRepository extends JpaRepository<UsuarioEntity, Long>{
+
+}
